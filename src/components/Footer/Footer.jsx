93ad32cff8b0c8e29b736c43554a770aa2a8d519
Lubna Fathima <lubnafathima275@gmail.com>
2024-07-01 18:32:21 +0530

    feat: added footer template

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      
+      <div className={styles.footer_socials}></div>
+      <div className={styles.footer_about}></div>
+      <div className={styles.footer_customer}></div>
+      <div className={styles.footer_help_center}></div>
     </div>
   )
 }
