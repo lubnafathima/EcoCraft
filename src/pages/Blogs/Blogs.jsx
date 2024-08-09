df07a1040b6b04073f74a0217562521e72204f17
Lubna Fathima <lubnafathima275@gmail.com>
2024-08-10 00:24:41 +0530

    feat: updated Blogs

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,11 +1,14 @@
 import styles from './Blogs.module.css';
 import Layout from "../Layout/Layout";
+import BlogsComponent from "../../components/BlogsComponent/BlogsComponent";
 
 const Blogs = () => {
   return (
     <div>
       <Layout>
-        <div className={styles.blogs}></div>
+        <div className={styles.blogs}>
+          <BlogsComponent />
+        </div>
       </Layout>
     </div>
   )
